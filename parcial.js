67aa790460a727438717fc0d2562fb260c786efa
LSVG <laura2162144@correo.uis.edu.co>
2020-07-27 22:05:17 -0500

    ya casi

--- parcial.js
+++ parcial.js
@@ -17,22 +17,42 @@ $(document).ready(
 	}
 );
 function editarC1(columna){
+
+
 	$("."+columna+">.texto").each(function(index){
-		//let text = $(this).text();
-		$(this).replaceWith("<td class='texto'><input value=" +$(this).text()+"></input></td>");
-		//$("<input value=" +text+"></input>").appendTo($(this));
+		$(this).replaceWith("<td class='texto'><input id='"+columna+"-"+index+"' value=" +$(this).text()+"></input></td>");
 	}
 	);
 	$("."+columna+">.emoticon").each(function(index){
-		//let text = $(this).text();
-		$(this).replaceWith("<td class='emoticon'><select><option value='bien'>bien</option><option value='mal'>mal</option></select></input></td>");
-		//$("<input value=" +text+"></input>").appendTo($(this));
+		$(this).replaceWith("<td class='emoticon'><select id='"+columna+"-Emoticon'><option value='bien'>bien</option><option value='mal'>mal</option></select></input></td>");
 	}
 	);
 	$("."+columna+">.editor").each(function(index){
-		//let text = $(this).text();
 		$(this).replaceWith("<td class='editando'>En edicion</input></td>");
-		//$("<input value=" +text+"></input>").appendTo($(this));
 	}
 	);
+	var parametro = "\""+columna+"\""
+	$("#espacio").append("<div>Pulse Aceptar para guardar los cambios o cancelar para anularlos</div>");
+	$("#espacio").append("<span><button id='aceptar' onclick='Aceptar("+parametro+")'>Aceptar</button></span>");
+	$("#espacio").append("<span><button id='cancelar' onclick='Cancelar()'>Cancelar</button></span>");
+		var nodoContenedorForm = document.getElementById('mostrarCambio');
+	nodoContenedorForm.innerHTML = 'Pulse Aceptar para guardar los cambios o cancelar para anularlos'+
+'<form name = "formulario" action="http://aprenderaprogramar.com" method="get" onsubmit="Aceptar('+parametro+')" onreset="anular()">';
+
+};
+function Aceptar(col){
+	var nodoContenedorForm = document.getElementById('mostrarCambio');
+	nodoContenedorForm.innerHTML = 'Pulse Aceptar para guardar los cambios o cancelar para anularlos'+
+'<form name = "formulario" action="http://aprenderaprogramar.com" method="get" onsubmit="capturarEnvio()" onreset="anular()">'+
+'<input type="hidden" name="alimento" value="'+document.querySelector('#'+col+'-0').value+'">'+
+'<input type="hidden" name="calorias" value="'+document.querySelector('#'+col+'-1').value+'">'+
+'<input type="hidden" name="grasas" value="'+document.querySelector('#'+col+'-2').value+'">'+
+'<input type="hidden" name="proteina" value="'+document.querySelector('#'+col+'-3').value+'">'+
+'<input type="hidden" name="carbohidratos" value="'+document.querySelector('#'+col+'-4').value+'">';
+document.formulario.submit();
+};
+function Cancelar(){
+
+window.location.reload();
+
 }
