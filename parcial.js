25529daed2362854a5ef04b0cfca511aa5568522
LSVG <laura2162144@correo.uis.edu.co>
2020-07-27 23:14:33 -0500

    probando

--- parcial.js
+++ parcial.js
@@ -1,10 +1,8 @@
-//var elementos = document.getElementsByClassName(emoticon);
+
+var editando=0;
 $(document).ready(
 	function CambiarEmojis(){
-		console.log('Hola');
-		console.log($(".columna1>td"));
-		//let emojis = document.querySelector('.emoticon');
-			//console.log(emojis);
+
 		$(".emoticon").each(function(index) {
       	console.log(index + ": " + $(this).text());
       		if ($(this).text() == 'bien') {
@@ -17,42 +15,44 @@ $(document).ready(
 	}
 );
 function editarC1(columna){
-
-
-	$("."+columna+">.texto").each(function(index){
-		$(this).replaceWith("<td class='texto'><input id='"+columna+"-"+index+"' value=" +$(this).text()+"></input></td>");
+	if(editando==0){
+		editando=1;
+		$("."+columna+">.texto").each(function(index){
+			$(this).replaceWith("<td class='texto'><input id='"+columna+"-"+index+"' value=" +$(this).text()+"></input></td>");
+		}
+		);
+		$("."+columna+">.emoticon").each(function(index){
+			$(this).replaceWith("<td class='emoticon'><select id='"+columna+"-Emoticon'><option value='bien'>bien</option><option value='mal'>mal</option></select></input></td>");
+		}
+		);
+		$("."+columna+">.editor").each(function(index){
+			$(this).replaceWith("<td class='editando'>En edicion</input></td>");
+		}
+		);
+		var parametro = "\""+columna+"\""
+		console.log($(""+columna+"-0").val());
+		var cambios = document.getElementById('mostrarCambio');
+		cambios.innerHTML = 'Pulse Aceptar para guardar los cambios o cancelar para anularlos'+
+		'<form name = "formulario" onsubmit="Aceptar('+parametro+')" onreset="anular()">'+
+		'<input class="boton" type = "submit" value="Aceptar"> <input class="boton" type="reset" value="Cancelar">';
+	}else{
+		alert('Solo se puede editar una línea. Recargue la página para poder editar otra')
 	}
-	);
-	$("."+columna+">.emoticon").each(function(index){
-		$(this).replaceWith("<td class='emoticon'><select id='"+columna+"-Emoticon'><option value='bien'>bien</option><option value='mal'>mal</option></select></input></td>");
-	}
-	);
-	$("."+columna+">.editor").each(function(index){
-		$(this).replaceWith("<td class='editando'>En edicion</input></td>");
-	}
-	);
-	var parametro = "\""+columna+"\""
-	$("#espacio").append("<div>Pulse Aceptar para guardar los cambios o cancelar para anularlos</div>");
-	$("#espacio").append("<span><button id='aceptar' onclick='Aceptar("+parametro+")'>Aceptar</button></span>");
-	$("#espacio").append("<span><button id='cancelar' onclick='Cancelar()'>Cancelar</button></span>");
-		var nodoContenedorForm = document.getElementById('mostrarCambio');
-	nodoContenedorForm.innerHTML = 'Pulse Aceptar para guardar los cambios o cancelar para anularlos'+
-'<form name = "formulario" action="http://aprenderaprogramar.com" method="get" onsubmit="Aceptar('+parametro+')" onreset="anular()">';
+	
+	
 
 };
 function Aceptar(col){
-	var nodoContenedorForm = document.getElementById('mostrarCambio');
-	nodoContenedorForm.innerHTML = 'Pulse Aceptar para guardar los cambios o cancelar para anularlos'+
-'<form name = "formulario" action="http://aprenderaprogramar.com" method="get" onsubmit="capturarEnvio()" onreset="anular()">'+
-'<input type="hidden" name="alimento" value="'+document.querySelector('#'+col+'-0').value+'">'+
-'<input type="hidden" name="calorias" value="'+document.querySelector('#'+col+'-1').value+'">'+
-'<input type="hidden" name="grasas" value="'+document.querySelector('#'+col+'-2').value+'">'+
-'<input type="hidden" name="proteina" value="'+document.querySelector('#'+col+'-3').value+'">'+
-'<input type="hidden" name="carbohidratos" value="'+document.querySelector('#'+col+'-4').value+'">';
-document.formulario.submit();
+	console.log($(""+col+"-0").val());
+	var datos={alimento:$(""+col+"-0").val(),
+			calorias:$(""+col+"-1").val(),
+			grasas:$(""+col+"-2").val(),
+			proteinas:$(""+col+"-3").val(),
+			carbohidratos:$(""+col+"-4").val()}
+//$.get("Formulario2", datos, procesar)
+//document.formulario.submit();
 };
+
 function Cancelar(){
-
 window.location.reload();
-
 }
